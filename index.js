a2fd3e7099cd0898872d14e34350222ef160af1d
mujemi26 <mohamed.kingzzz@gmail.com>
2024-07-12 23:45:05 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var menuButton = document.getElementById("menu-btn");
+var sideNav = document.getElementById("sidenav");
+var menu = document.getElementById("menu");
+
+menuButton.onclick = function () {
+  if (sideNav.style.right == "-250px") {
+    sideNav.style.right = "0";
+    menu.src = "/images/closee.png";
+  } else {
+    sideNav.style.right = "-250px";
+    menu.src = "images/main-menu.png";
+  }
+};
+
+// All animations will take exactly 500ms
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
